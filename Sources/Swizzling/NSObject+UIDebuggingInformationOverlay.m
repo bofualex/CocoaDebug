1c54815454dd957adcb67db0f791f4f83561476a
Alex Bofu <>
2018-07-27 12:11:07 +0300

    importing cocoa debug

--- Sources/Swizzling/NSObject+UIDebuggingInformationOverlay.m
+++ Sources/Swizzling/NSObject+UIDebuggingInformationOverlay.m
@@ -14,6 +14,7 @@
 @import UIKit;
 @import Foundation;
 @import ObjectiveC.runtime;
+#import <CocoaDebug/CocoaDebug-Swift.h> 
 
 @interface NSObject()
 - (void)_setWindowControlsStatusBarOrientation:(BOOL)orientation;
